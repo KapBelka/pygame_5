3bc7c993c6fe8a81df4bf4224c28222c3668efca
Belka <lep12345@mail.ru>
2019-12-10 23:19:58 +0700

    Game over

--- asd.py
+++ asd.py
@@ -27,27 +27,33 @@ def gameover():
 
 pygame.init()
 screen = pygame.display.set_mode(SCREEN_SIZE)
-fps = 30
-gameover_image = load_image('gameover.png')
 all_sprites = pygame.sprite.Group()
-GAMEOVER_EVENT = 30
+gameover_image = load_image('gameover.png')
+# переменные
+fps = 30
 gameover_speed = 200
-pygame.time.set_timer(GAMEOVER_EVENT, 5000)
 running = True
+is_gameover = False
+# Создаём событие gameover
+GAMEOVER_EVENT_ID = pygame.USEREVENT + 1
+gameover_event = pygame.event.Event(GAMEOVER_EVENT_ID, {})
+pygame.event.post(gameover_event)  # Добавляем наше событие в очередь
+# Функции pygame
 pygame.mouse.set_visible(False)
 clock = pygame.time.Clock()
-gameover_screen = None
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        if event.type == GAMEOVER_EVENT:
-            gameover_screen = gameover()
+        if event.type == GAMEOVER_EVENT_ID:  # Обрабатывем наше собыите
+            gameover_screen = gameover()  # Создаём спрайт с изображением gameover
+            is_gameover = True  # Показываем что игра окончена
     screen.fill(pygame.Color('blue'))
-    if gameover_screen:
-        gameover_screen.rect.x += gameover_speed / fps
-        if gameover_screen.rect.right >= 600:
-            gameover_screen = None
+    if is_gameover:
+        gameover_screen.rect.x += gameover_speed / fps  # Двигаем спрайт gameover
+        if gameover_screen.rect.right >= SCREEN_SIZE[0]:  # Как мы доходим до края экрана
+            gameover_screen.rect.topleft = (0, 0)  # Поставить спрайт в начале координат
+            is_gameover = False  # Отключаем этот блок кода
     all_sprites.draw(screen)
     clock.tick(fps)
     pygame.display.update()
